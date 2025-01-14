06180603f0dae4f404abeb80da898685e802593e
William <william.jewers@gmail.com>
2025-01-14 18:15:21 +0000

    Add src/data_analysis.py

---
+++ src/data_analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from textblob import TextBlob
+
+# Sample sentiment analysis function
+
+def analyze_sentiment(text):
+    analysis = TextBlob(text)
+    if analysis.sentiment.polarity > 0:
+        return 'Positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'Neutral'
+    else:
+        return 'Negative'
